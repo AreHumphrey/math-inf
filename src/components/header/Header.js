430d9acaf9e589244f7026ab5fe37bff558140e3
humphrey <lutdina04@yandex.ru>
2024-06-04 15:48:26 +1000

    fix dropdown menu

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,11 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, {useState, useRef, useEffect} from 'react';
 import './Header.css';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 const Header = () => {
     const [activeMenu, setActiveMenu] = useState(null);
     const [dropdownDirection, setDropdownDirection] = useState('down');
+    const [activeSubmenu, setActiveSubmenu] = useState(null);
     const dropdownRef = useRef(null);
 
     const handleMouseEnter = (menu) => {
@@ -22,7 +23,16 @@ const Header = () => {
 
     const handleMouseLeave = () => {
         setActiveMenu(null);
+        setActiveSubmenu(null);
         setDropdownDirection('down');
+    };
+
+    const handleSubmenuEnter = (submenu) => {
+        setActiveSubmenu(submenu);
+    };
+
+    const handleSubmenuLeave = () => {
+        setActiveSubmenu(null);
     };
 
     const menus = {
@@ -32,7 +42,12 @@ const Header = () => {
         Информатика: ['7 класс', '8 класс', '9 класс', '10 класс', '11 класс'],
         ОГЭ: ['математика', 'информатика'],
         ЕГЭ: ['математика (база)', 'математика (профиль)', 'информатика'],
-        ГДЗ: ['математика', 'алгебра', 'геометрия', 'информатика']
+        ГДЗ: {
+            математика: ['5 класс', '6 класс'],
+            алгебра: ['7 класс', '8 класс', '9 класс', '10 класс', '11 класс'],
+            геометрия: ['7 класс', '8 класс', '9 класс', '10 класс', '11 класс'],
+            информатика: ['7 класс', '8 класс', '9 класс', '10 класс', '11 класс']
+        }
     };
 
     return (
@@ -57,11 +72,31 @@ const Header = () => {
                                 {menu}
                                 {activeMenu === menu && (
                                     <div className={`dropdown-menu ${dropdownDirection}`} ref={dropdownRef}>
-                                        {menus[menu].map((item, index) => (
-                                            <div key={index} className="dropdown-item">
-                                                {item}
-                                            </div>
-                                        ))}
+                                        {typeof menus[menu] === 'object' && !Array.isArray(menus[menu])
+                                            ? Object.keys(menus[menu]).map((submenu, index) => (
+                                                <div
+                                                    key={index}
+                                                    className="dropdown-item"
+                                                    onMouseEnter={() => handleSubmenuEnter(submenu)}
+                                                    onMouseLeave={handleSubmenuLeave}
+                                                >
+                                                    {submenu}
+                                                    {activeSubmenu === submenu && (
+                                                        <div className="dropdown-submenu">
+                                                            {menus[menu][submenu].map((item, subIndex) => (
+                                                                <div key={subIndex} className="dropdown-subitem">
+                                                                    {item}
+                                                                </div>
+                                                            ))}
+                                                        </div>
+                                                    )}
+                                                </div>
+                                            ))
+                                            : menus[menu].map((item, index) => (
+                                                <div key={index} className="dropdown-item">
+                                                    {item}
+                                                </div>
+                                            ))}
                                     </div>
                                 )}
                             </li>
@@ -69,7 +104,7 @@ const Header = () => {
                     </ul>
                 </nav>
                 <div className="profile-icon">
-                    <a href="/profile"><AccountCircleIcon /></a>
+                    <a href="/profile"><AccountCircleIcon/></a>
                 </div>
             </header>
         </>
